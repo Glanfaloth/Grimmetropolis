1cc7305ad2acc7bc75836e98877c71552a9b519e
Your Name <yeltao@ethz.ch>
2021-03-09 13:35:45 +0100

    tile map and camera

---
+++ Source/Grimmetropolis/MainGame.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Grimmetropolis
+{
+    public class MainGame : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Map myMap;
+        public MainGame()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+            myMap = new Map(GraphicsDevice, 32, 32, 10, 10);
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            Vector2 moveVelocity = Vector2.Zero;
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                moveVelocity += new Vector2(1, 0);
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                moveVelocity += new Vector2(-1, 0);
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                moveVelocity += new Vector2(0, -1);
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                moveVelocity += new Vector2(0, 1);
+            }
+
+            myMap.Camera.Move(moveVelocity);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            myMap.draw(_spriteBatch);
+
+            base.Draw(gameTime);
+        }
+    }
+}
